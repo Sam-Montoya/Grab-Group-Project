043c5d1bbcb3739c3226c402378c37c39cfef1cd
Sam-Montoya <samuel.montoya1073@gmail.com>
2017-10-16 14:41:01 -0600

    Fixed a couple errors.

--- server/GetController/getController.js
+++ server/GetController/getController.js
@@ -12,17 +12,17 @@ export default class getController {
     //This is ran when the user first logs in (user info being stored on redux)
     getInitialUserInfo() {
 
-    },
+    }
 
     //This is called when clicking on a listing (user listing info)
     getUserInfo() {
 
-    },
+    }
 
     //This should be called when the user updates their favorites (called to get the new updated list)
     getUserFavorites() {
 
-    },
+    }
 
     //This is called when viewing the user's chats
     getUserChats() {
