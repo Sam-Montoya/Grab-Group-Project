d5550125d83d6014dfdfa7ec11b9bd6bab3b343d
Sam-Montoya <samuel.montoya1073@gmail.com>
2017-10-16 16:32:15 -0600

    Added some get stuff.

--- server/GetController/getController.js
+++ server/GetController/getController.js
@@ -2,31 +2,28 @@
  * This controller holds all the get requests
  */
 
-export default class getController {
-    constructor(props) {
-        super(props);
+module.exports = {
+	constructor(props) {
+		console.log(props);
+	},
 
-        console.log(props);
-        // let DB = this.props.DB;
-    }
-    //This is ran when the user first logs in (user info being stored on redux)
-    getInitialUserInfo() {
+	//This is ran when the user first logs in (user info being stored on redux)
+	getInitialUserInfo() {
+		console.log('Hit initial user info!');
+	},
 
-    }
+	//This is called when clicking on a listing (user listing info)
+	getUserInfo() {
+		console.log('Hit get user info!');
+	},
 
-    //This is called when clicking on a listing (user listing info)
-    getUserInfo() {
+	//This should be called when the user updates their favorites (called to get the new updated list)
+	getUserFavorites() {
+		console.log('Hit get user listings!');
+	},
 
-    }
-
-    //This should be called when the user updates their favorites (called to get the new updated list)
-    getUserFavorites() {
-
-    }
-
-    //This is called when viewing the user's chats
-    getUserChats() {
-
-    }
-
+	//This is called when viewing the user's chats
+	getUserChats() {
+		console.log('Hit get user chats!');
+	}
 }
--- server/server.js
+++ server/server.js
@@ -1,77 +1,124 @@
 require('dotenv').config();
 
 const express = require('express'),
-    bodyParser = require('body-parser'),
-    passport = require('passport'),
-    Auth0Strategy = require('passport-auth0'),
-    massive = require('massive'),
-    session = require('express-session');
+	bodyParser = require('body-parser'),
+	passport = require('passport'),
+	Auth0Strategy = require('passport-auth0'),
+	massive = require('massive'),
+	session = require('express-session'),
+	axios = require('axios');
 
-const app = express();
+let getController = require('./GetController/getController.js')
+
+let app = express();
 app.use(bodyParser.json());
 
-app.use(session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true
-}));
+massive(process.env.CONNECTIONSTRING)
+	.then((db) => {
+		app.set('db', db);
+		console.log('Connected successfully.');
+	})
+	.catch((err) => console.log('Something happened... ' + err));
+
+/**
+ * Endpoints
+ */
+
+// -- Get Requests
+let db = app.get('db');
+getController.constructor(db);
+app.get('/api/getInitialUserInfo', (request, response) => {
+	getController.getInitialUserInfo();
+});
+app.get('/api/getUserInfo', (request, response) => {
+	getController.getUserInfo();
+});
+app.get('/api/getUserListings', (request, response) => {});
+app.get('/api/getUserChats', (request, response) => {});
+
+// -- Post Requests
+
+// -- Put Requests
+
+// -- Delete Requests
+
+/**
+ * Endpoints End
+ */
+
+/**
+ * Auth 0
+ */
+
+app.use(
+	session({
+		secret: process.env.SECRET,
+		resave: false,
+		saveUninitialized: true
+	})
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-massive(process.env.CONNECTIONSTRING).then(db => {
-    app.set('db', db);
+passport.use(
+	new Auth0Strategy(
+		{
+			domain: process.env.AUTH_DOMAIN,
+			clientID: process.env.AUTH_CLIENT_ID,
+			clientSecret: process.env.AUTH_CLIENT_SECRET,
+			callbackURL: process.env.AUTH_CALLBACK
+		},
+		function(accessToken, refreshToken, extraParams, profile, done) {
+			// app.get('db').find_user(profile.id).then((user) => {
+			// 	if (user[0]) {
+			// 		done(null, user);
+			// 	} else {
+			// 		console.log(profile); //debug
+			// 		let fullname = profile.name.givenName + " " + profile.name.familyName;
+			// 		app.get('db').create_user([profile.id, fullname, profile.username, profile.picture, profile.emails[0].value]).then(user => {
+			// 		    done(null, user[0]);
+			// 		});
+			// 		done(null, profile);
+			// 	}
+			// });
+		}
+	)
+);
+
+passport.serializeUser(function(user, done) {
+	done(null, user);
 });
 
-passport.use(new Auth0Strategy({
-    domain: process.env.AUTH_DOMAIN,
-    clientID: process.env.AUTH_CLIENT_ID,
-    clientSecret: process.env.AUTH_CLIENT_SECRET,
-    callbackURL: process.env.AUTH_CALLBACK
-},
-    function (accessToken, refreshToken, extraParams, profile, done) {
-        app.get('db').find_user(profile.id).then(user => {
-            if (user[0]) {
-                done(null, user);
-            } else {
-                console.log(profile); //debug
-                // let fullname = profile.name.givenName + " " + profile.name.familyName;
-                // app.get('db').create_user([profile.id, fullname, profile.username, profile.picture, profile.emails[0].value]).then(user => {
-                //     done(null, user[0]);
-                // });
-                done(null, profile);
-            }
-        });
-    }
-));
-
-passport.serializeUser(function (user, done) {
-    done(null, user);
-});
-
-passport.deserializeUser(function (user, done) {
-    done(null, user);
+passport.deserializeUser(function(user, done) {
+	done(null, user);
 });
 
 app.get('/auth', passport.authenticate('auth0'));
 
-app.get('/auth/callback', passport.authenticate('auth0', {
-    successRedirect: 'http://localhost:3000/#/',
-    failureRedirect: '/'
-}));
+app.get(
+	'/auth/callback',
+	passport.authenticate('auth0', {
+		successRedirect: 'http://localhost:3000/#/',
+		failureRedirect: '/'
+	})
+);
 
 app.get('/auth/me', (req, res, next) => {
-    if (!req.user) {
-        res.status(200).send("User not found");
-    } else {
-        res.status(200).send(req.user);
-    }
+	if (!req.user) {
+		res.status(200).send('User not found');
+	} else {
+		res.status(200).send(req.user);
+	}
 });
 
 app.get('/auth/logout', (req, res) => {
-    req.logOut();
-    res.redirect(302, 'http://localhost:3000/#/');
+	req.logOut();
+	res.redirect(302, 'http://localhost:3000/#/');
 });
+/**
+ * Auth 0 End
+ */
 
 const port = 3060;
 app.listen(port, console.log(`It's lit on ${port} fam!`));
