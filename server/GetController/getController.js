16a4a79ffc1fb567b0aea549ad8fa134462590f4
Sam-Montoya <samuel.montoya1073@gmail.com>
2017-10-16 14:39:18 -0600

    Added getController stuff.

---
+++ server/GetController/getController.js
@@ -0,0 +1,32 @@
+/**
+ * This controller holds all the get requests
+ */
+
+export default class getController {
+    constructor(props) {
+        super(props);
+
+        console.log(props);
+        // let DB = this.props.DB;
+    }
+    //This is ran when the user first logs in (user info being stored on redux)
+    getInitialUserInfo() {
+
+    },
+
+    //This is called when clicking on a listing (user listing info)
+    getUserInfo() {
+
+    },
+
+    //This should be called when the user updates their favorites (called to get the new updated list)
+    getUserFavorites() {
+
+    },
+
+    //This is called when viewing the user's chats
+    getUserChats() {
+
+    }
+
+}
