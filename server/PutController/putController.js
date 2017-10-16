8fa20f88a807ddf601b4f6d8e87345bed9c474b6
StayinNasty <keganswensen@gmail.com>
2017-10-16 14:59:24 -0600

    Created the barebones of the other three controllers

--- server/PutController/putController.js
+++ server/PutController/putController.js
@@ -1,1 +1,17 @@
-//test
+/**
+ * This controller holds all the put requests
+ */
+
+export default class putController {
+    constructor(props) {
+        super(props);
+
+        console.log(props);
+        // let DB = this.props.DB;
+    }
+    // Updates the User info
+    updateUserInfo(){
+        
+    }
+
+}
