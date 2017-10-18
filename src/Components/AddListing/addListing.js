4c4875a61b288e8ccefc7e9c873a9b98a6c3bd53
StayinNasty <keganswensen@gmail.com>
2017-10-18 17:43:14 -0600

    Added the uploading feature for multiple images

--- src/Components/AddListing/addListing.js
+++ src/Components/AddListing/addListing.js
@@ -1,16 +1,73 @@
 import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
+import request from 'superagent';
+import './test.css';
 
-class AddListing extends Component{
-    constructor(){
+const CLOUDINARY_UPLOAD_PRESET = 'v7tjejit';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/grab/upload';
+
+class AddListing extends Component {
+    constructor() {
         super()
-        this.state={
-
+        this.state = {
+            uploadedFileCloudinaryUrl: [],
+            uploadedFile: []
         }
     }
-    render(){
-        return(
+
+    onImageDrop(files) {
+        this.setState({
+            uploadedFile: files
+        });
+
+        this.handleImageUpload(files);
+    }
+
+    handleImageUpload(file) {
+        for (let i = 0; i < file.length; i++) {
+            let upload = request.post(CLOUDINARY_UPLOAD_URL)
+                .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+                .field('file', file[i]);
+
+            upload.end((err, response) => {
+                if (err) {
+                    console.error(err);
+                }
+
+                if (response.body.secure_url !== '') {
+                    this.setState({
+                        uploadedFileCloudinaryUrl: [... this.state.uploadedFileCloudinaryUrl, response.body.secure_url]
+                    });
+                }
+            });
+        }
+    }
+
+    render() {
+        let mappedImages = this.state.uploadedFileCloudinaryUrl.map((image, i) => {
+            return <div key={i}>
+                <img src={image} />
+            </div>
+        })
+        return (
             <div>
                 <h1>Add Listing page</h1>
+                <div className='test'>
+                    <Dropzone
+                        multiple={true}
+                        accept="image/*"
+                        onDrop={this.onImageDrop.bind(this)}>
+                        <p>Drop an image or click to select a file to upload.</p>
+                    </Dropzone>
+                </div>
+                <div>
+                    {this.state.uploadedFileCloudinaryUrl === '' 
+                    ? 
+                    null 
+                    :
+                    mappedImages 
+                    }
+                </div>
             </div>
         )
     }
