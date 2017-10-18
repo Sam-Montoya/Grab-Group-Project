6189f826cf2e79ff91232979c968cb536667a94f
Ben Ahlander <ben.ahlander@gmail.com>
2017-10-18 09:56:45 -0600

    updated front end

--- src/Components/ListingInfo/listingInfo.js
+++ src/Components/ListingInfo/listingInfo.js
@@ -4,8 +4,12 @@ import Paper from 'material-ui/Paper';
 import Avatar from 'material-ui/Avatar';
 import Button from 'material-ui/Button';
 import List, { ListItem, ListItemText } from 'material-ui/List';
-import Inbox from 'material-ui-icons/Inbox';
+import Inbox from 'material-ui-icons/Message';
 import Pageview from 'material-ui-icons/Pageview';
+import Star from 'material-ui-icons/Star';
+import Person from 'material-ui-icons/Person';
+import Back from 'material-ui-icons/KeyboardBackspace';
+
 
 
 class ListingInfo extends Component {
@@ -17,50 +21,97 @@ class ListingInfo extends Component {
     }
     render() {
         const img = require('../../images/benMt.1866739e.jpg')
+        const twitter = require('../../images/TwitterLogo2.png')
+        const gmail = require('../../images/mailLogo.png')
+        const faceboook = require('../../images/fbLogo.png')
+        const xbox = require('../../images/xbox.jpg')
         return (
             <div className="ListingPage">
                 <div className="sidebar">
                     <div className="leftBar">
-                        <Avatar alt="Remy Sharp" src={img} style={{ width: '60px', height: '60px', margin: 'auto', marginTop: '20px' }} />
-                        <p>Price: $255</p>
-                        <Button raised>
-                            Inbox
-                        </Button>
-                        <Button raised>
-                            My Listings
-                        </Button>
-                        <Button raised>
-                            My Favorites
-                        </Button>
-
-                        <List>
-                            <ListItem button>
-                                <Avatar style={{backgroundColor:'lightblue', width:'30px', height:'30px'}}>
-                                    <Inbox />
-                                </Avatar>
-                                <ListItemText primary="Inbox" />
-                            </ListItem>
-                            <ListItem button>
-                                <Avatar style={{backgroundColor:'purple', width:'30px', height:'30px'}}>
-                                    <Pageview />
-                                </Avatar>
-                                <ListItemText primary="Listings"/>
-                            </ListItem>
-                        </List>
+                        <div className="topMenuThing">
+                            <a href="#">
+                                <div style={{ backgroundColor: 'white', width: '100%', height: '80px', }}>
+                                    <Avatar style={{ backgroundColor: '#607D8B' }}>
+                                        <Back />
+                                    </Avatar>
+                                </div>
+                            </a>
+                            <a href="#">
+                                <div style={{ backgroundColor: '#EF9A9A', width: '100%', height: '80px', }}>
+                                    <Avatar style={{ backgroundColor: '#C62828' }}>
+                                        <Person />
+                                    </Avatar>
+                                </div>
+                            </a>
+                            <a href="#">
+                                <div style={{ backgroundColor: 'lightblue', width: '100%', height: '80px', }}>
+                                    <Avatar style={{ backgroundColor: 'navy' }}>
+                                        <Inbox />
+                                    </Avatar>
+                                </div>
+                            </a>
+                            <a href="#">
+                                <div style={{ backgroundColor: '#FF9800', width: '100%', height: '80px', }}>
+                                    <Avatar style={{ backgroundColor: '#E65100' }}>
+                                        <Star />
+                                    </Avatar>
+                                </div>
+                            </a>
+                        </div>
+                        <h3>Price: $255</h3>
+                        <hr />
+                        <h3>Favorited: 23</h3>
+                        <hr />
+                        <h3>(812)234-2343</h3>
+                        <div className="logos">
+                            <img src={faceboook} />
+                            <img src={twitter} />
+                            <img src={gmail} />
+                        </div>
+                        <hr />
+                        <div>
+                            <div className="moreFromThisSellerContainer">
+                                <h3>More from this seller</h3>
+                                <div className="moreFromThisSeller">
+                                    <img src={xbox} />
+                                    <caption>Xbox 360</caption>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="ListingInfoContainer">
-                    <Paper style={{ width: '400px' }}>
+                    <Paper
+                        className="half1"
+                    >
                         <ListingImages />
                     </Paper>
-                    <Paper style={{
-                        width: '400px',
-                        paddingTop: '0px',
-                        textAlign: 'center'
-                    }}>
-                        <h1>Bens Xbox</h1>
+                    <Paper
+                        className="half"
+                    >
+                        <h3>Bens Xbox</h3>
                         <h3>Awesome condidtion</h3>
                         <p>Its really cool</p>
+                    </Paper>
+                    <Paper className="full">
+                        <h3>Description</h3>
+                        <p>bla bla bal</p>
+                        <p>bla bla</p>
+                        <p>hello world</p>
+                    </Paper>
+                    <Paper
+                        className="half1"
+                    >
+                        <h3>Pros</h3>
+                        <h3>Awesome condidtion</h3>
+                        <p>Its really cool</p>
+                    </Paper>
+                    <Paper className="half"
+                    >
+                        <h3>Cons</h3>
+                        <h3>Jk its broken</h3>
+                        <p>Nothing works</p>
                     </Paper>
                 </div>
             </div>
