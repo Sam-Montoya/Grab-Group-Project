fc3cf9c20f616b8a84fd612698eb0ab61ac51b79
Sam-Montoya <samuel.montoya1073@gmail.com>
2017-10-19 16:10:12 -0600

    Pictures have been reverted

--- src/Components/ListingInfo/listingImages.js
+++ src/Components/ListingInfo/listingImages.js
@@ -42,60 +42,49 @@ class TextMobileStepper extends React.Component {
 	};
 
 	render() {
-		const classes = this.props.classes;
-		let pictures;
-		if (this.props.images) {
-			pictures = this.props.images.map((image, i) => {
-				return (
-					<div className={classes.root}>
-						<Paper square elevation={0} className={classes.header}>
-							<Typography> Picture {this.state.activeStep + 1} of 3</Typography>
-						</Paper>
-						{this.state.activeStep === 0 ? (
-							<div className="imgContainer">
-								<img src={image} />
-							</div>
-						) : this.state.activeStep === 1 ? (
-							<div className="imgContainer">
-								<img src="" />
-							</div>
-						) : (
-							<div className="imgContainer">
-								<img src="http://www.omarimc.com/wp-content/uploads/2017/06/video-games-893225_960_720.jpg" />
-							</div>
-						)}
-					</div>
-				);
-			});
-		} else {
-			return <div />;
-		}
+        const classes = this.props.classes;
+        return (
+            <div className={classes.root}>
+                <Paper square elevation={0} className={classes.header}>
+                    <Typography> Picture {this.state.activeStep + 1} of 3</Typography>
+                </Paper>
+                {
+                    (this.state.activeStep === 0) ? <div className="imgContainer">
+                        <img src="https://s.yimg.com/ny/api/res/1.2/tQKmRJ1OYtRWStip4MacXg--/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9NjAwO2g9NDAw/https://cdn2.benzinga.com/files/imagecache/600x400xUP/images/story/2012/console-playstation-xbox-joystick-video-games-1216816.jpeg" />
+                    </div>
+                        :
+                        (this.state.activeStep === 1) ?
+                            <div className="imgContainer">
+                                <img src="https://pisces.bbystatic.com/BestBuy_US/store/ee/2016/vg/pol/evn-xbox-one-s-0821.jpg;maxHeight=288;maxWidth=520" />
+                            </div>
+                            :
+                            <div className="imgContainer">
+                                <img src="http://www.omarimc.com/wp-content/uploads/2017/06/video-games-893225_960_720.jpg" />
+                            </div>
+                }
 
-		return (
-			<div>
-				<MobileStepper
-					type="dots"
-					steps={3}
-					position="static"
-					activeStep={this.state.activeStep}
-					style={{ backgroundColor: 'white' }}
-					nextButton={
-						<Button dense onClick={this.handleNext} disabled={this.state.activeStep === 2}>
-							Next
-							<KeyboardArrowRight />
-						</Button>
-					}
-					backButton={
-						<Button dense onClick={this.handleBack} disabled={this.state.activeStep === 0}>
-							<KeyboardArrowLeft />
-							Back
-						</Button>
-					}
-				/>
-				{pictures}
-			</div>
-		);
-	}
+                <MobileStepper
+                    type="dots"
+                    steps={3}
+                    position="static"
+                    activeStep={this.state.activeStep}
+                    style={{ backgroundColor: 'white' }} 
+                    nextButton={
+                        <Button dense onClick={this.handleNext} disabled={this.state.activeStep === 2}>
+                            Next
+                 <KeyboardArrowRight />
+                        </Button>
+                    }
+                    backButton={
+                        <Button dense onClick={this.handleBack} disabled={this.state.activeStep === 0}>
+                            <KeyboardArrowLeft />
+                            Back
+                         </Button>
+                    }
+                />
+            </div>
+        );
+    }
 }
 
 TextMobileStepper.propTypes = {
@@ -103,3 +92,17 @@ TextMobileStepper.propTypes = {
 };
 
 export default withStyles(styles)(TextMobileStepper);
+
+// {this.state.activeStep === 0 ? (
+// 	<div className="imgContainer">
+// 		<img src={image} />
+// 	</div>
+// ) : this.state.activeStep === 1 ? (
+// 	<div className="imgContainer">
+// 		<img src="" />
+// 	</div>
+// ) : (
+// 	<div className="imgContainer">
+// 		<img src="http://www.omarimc.com/wp-content/uploads/2017/06/video-games-893225_960_720.jpg" />
+// 	</div>
+// )}
