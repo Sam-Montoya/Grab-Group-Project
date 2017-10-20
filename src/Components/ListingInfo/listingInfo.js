53f02d5bfde95c145b356ff891fe19c9694d0bc6
Sam-Montoya <samuel.montoya1073@gmail.com>
2017-10-20 14:03:58 -0600

    Fixed big favorites bug

--- src/Components/ListingInfo/listingInfo.js
+++ src/Components/ListingInfo/listingInfo.js
@@ -60,30 +60,30 @@ class ListingInfo extends Component {
 	}
 
 	addListingToFavorites() {
-		const config = { listing_id: this.state.listingInfo.listing_id, user_id: this.props.user.user_id }
-		axios.post('/api/addFavorite', config)
-			.then((response) => {
-				this.props.getUserFavorites(this.props.user.user_id);
-				this.favoriteIcon();
-				alert('Added to Favorites!');
-			});
+		const config = { listing_id: this.state.listingInfo.listing_id, user_id: this.props.user.user_id };
+		axios.post('/api/addFavorite', config).then((response) => {
+			this.props.getUserFavorites(this.props.user.user_id);
+			this.favoriteIcon();
+			alert('Added to Favorites!');
+		});
 	}
 
 	removeFavorite() {
 		if (this.props.user) {
-			axios.delete(`/api/removeFavorite/${this.state.listingInfo.listing_id}/${this.props.user.user_id}`)
+			axios
+				.delete(`/api/removeFavorite/${this.state.listingInfo.listing_id}/${this.props.user.user_id}`)
 				.then((response) => {
 					this.props.getUserFavorites(this.props.user.user_id);
 					this.favoriteIcon();
 					alert('Listing has been removed from your favorites!');
-				})
+				});
 		}
 	}
 
 	checkFavorites() {
 		let existing = this.props.favorites.filter((listing, i) => {
 			return listing.listing_id === this.state.listingInfo.listing_id;
-		})
+		});
 		if (existing.length) {
 			return true;
 		} else {
@@ -97,7 +97,12 @@ class ListingInfo extends Component {
 				<div className="sidebar">
 					<div className="leftBar">
 						<div className="topMenuThing">
-							<div style={{ backgroundColor: 'white', width: '100%', height: '80px' }} className='button_cursor' onClick={() => { window.history.back() }}>
+							<div
+								style={{ backgroundColor: 'white', width: '100%', height: '80px' }}
+								className="button_cursor"
+								onClick={() => {
+									window.history.back();
+								}}>
 								<Avatar style={{ backgroundColor: '#607D8B' }}>
 									<Back />
 								</Avatar>
@@ -114,7 +119,6 @@ class ListingInfo extends Component {
 							</div>
 							<div className="button_cursor">
 								<div style={{ backgroundColor: '#FF9800', width: '100%', height: '80px' }}>
-
 									<this.favoriteIcon />
 								</div>
 							</div>
@@ -147,8 +151,8 @@ class ListingInfo extends Component {
 						{this.state.listingInfo.images.length !== 0 ? (
 							<ListingImages images={this.state.listingInfo.images} />
 						) : (
-								<div>No Images</div>
-							)}
+							<div>No Images</div>
+						)}
 					</Paper>
 					<Paper className="half">
 						<h3>{this.state.listingInfo.title}</h3>
@@ -173,32 +177,54 @@ class ListingInfo extends Component {
 	favoriteIcon() {
 		let favoriteIcon = (
 			<Avatar style={{ backgroundColor: 'white' }}>
-				<Star style={{ color: '#FFFF00' }}
-					onClick={() => { alert('Please Log In') }}
+				<Star
+					style={{ color: '#FFFF00' }}
+					onClick={() => {
+						alert('Please Log In');
+					}}
 				/>
 			</Avatar>
 		);
 
 		if (this.props.user) {
-			for (let j = 0; j < this.props.favorites.length; j++) {
-				if (this.props.favorites[j].listing_id === this.state.listingInfo.listing_id) {
-					favoriteIcon = (
-						<Avatar style={{ backgroundColor: 'white' }}>
-							<Star style={{ color: '#FFFF00' }}
-								onClick={() => { this.removeFavorite() }}
-							/>
-						</Avatar>
-					)
-					break;
-				} else {
-					favoriteIcon = (
-						<Avatar style={{ backgroundColor: 'black' }}>
-							<Star style={{ color: 'black' }}
-								onClick={() => { this.addListingToFavorites() }}
-							/>
-						</Avatar>
-					)
+			if (this.props.favorites.length) {
+				for (let j = 0; j < this.props.favorites.length; j++) {
+					if (this.props.favorites[j].listing_id === this.state.listingInfo.listing_id) {
+						favoriteIcon = (
+							<Avatar style={{ backgroundColor: 'white' }}>
+								<Star
+									style={{ color: '#FFFF00' }}
+									onClick={() => {
+										this.removeFavorite();
+									}}
+								/>
+							</Avatar>
+						);
+						break;
+					} else {
+						favoriteIcon = (
+							<Avatar style={{ backgroundColor: 'black' }}>
+								<Star
+									style={{ color: 'black' }}
+									onClick={() => {
+										this.addListingToFavorites();
+									}}
+								/>
+							</Avatar>
+						);
+					}
 				}
+			} else {
+				favoriteIcon = (
+					<Avatar style={{ backgroundColor: 'black' }}>
+						<Star
+							style={{ color: 'black' }}
+							onClick={() => {
+								this.addListingToFavorites();
+							}}
+						/>
+					</Avatar>
+				);
 			}
 		}
 		return favoriteIcon;
