e7afdb69de6981dbd465de0b361ffcecc81272cc
Anthony Montoya <a.montoya1073@gmail.com>
2017-11-02 20:39:39 -0600

    Unit Testing is complete by all members

--- src/testing/functions.js
+++ src/testing/functions.js
@@ -1,12 +1,8 @@
 const axios = require('axios')
 
 module.exports = {
-    returnTwo: function(){
-        return 2
-    }, 
-
     handleCreate: (listingObj) => {
-        if(listingObj){
+        if (listingObj) {
             return true
         } else {
             return false
@@ -14,17 +10,69 @@ module.exports = {
     },
 
     getUserFavorites: (user_id) => {
-	if (user_id) {
-		return {
-			type: "GET_USER_FAVORITES",
-			payload: ["someStuff", "moreStuff", "evenMoreStuff"]
-		};
-	} else {
-		return {
-			type: "GET_USER_FAVORITES",
-			payload: []
-		};
-	}
-}
-    
+        if (user_id) {
+            return {
+                type: "GET_USER_FAVORITES",
+                payload: ["someStuff", "moreStuff", "evenMoreStuff"]
+            };
+        } else {
+            return {
+                type: "GET_USER_FAVORITES",
+                payload: []
+            };
+        }
+    },
+
+    handleDelete: (listingObj) => {
+        if (listingObj) {
+            return true
+        } else {
+            return false
+        }
+    },
+
+    updateSearchTerm: (word) => {
+        if (word) {
+            return {
+                type: "UPDATE_SEARCH_TERM",
+                payload: word
+            };
+        } else {
+            return {
+                type: "UPDATE_SEARCH_TERM",
+                payload: ''
+            };
+        }
+    },
+
+    testSwitchCase: (backgroundColor) => {
+        if (backgroundColor === 'rgba(53, 138, 255') {
+            return 'Electronics'
+        } else if (backgroundColor === 'rgba(147, 74, 255') {
+            return 'Home'
+        } else if (backgroundColor === 'rgba(104, 208, 52') {
+            return 'Sports'
+        } else if (backgroundColor === 'rgba(151, 151, 151') {
+            return 'Parts'
+        } else if (backgroundColor === 'rgba(255, 127, 127') {
+            return 'Free'
+        } else if (backgroundColor === 'rgba(0, 255, 255, 0.68') {
+            return 'Default'
+        }
+    },
+
+    updateFilter: (filter) => {
+        if (filter === 'Electronics') {
+            return 'Electronics'
+        } else if (filter === 'Home') {
+            return 'Home'
+        } else if (filter === 'Sports') {
+            return 'Sports'
+        } else if (filter === 'Parts') {
+            return 'Parts'
+        } else if (filter === 'Free') {
+            return 'Free'
+        }
+    }
+
 }
--- src/testing/functions.test.js
+++ src/testing/functions.test.js
@@ -1,19 +1,14 @@
 const functions = require('./functions')
 
 const GET_USER_FAVORITES = "GET_USER_FAVORITES";
-
-describe("returnTwo", () => {
-    test("returnTwo() should return 2", () => {
-        expect(functions.returnTwo()).toEqual(2)
-    })
-})
+const UPDATE_SEARCH_TERM = 'UPDATE_SEARCH_TERM';
 
 describe("handleCreate", () => {
-    test ("handleCreate(listingObj) should return true", () => {
+    test("handleCreate(listingObj) should return true", () => {
         expect(functions.handleCreate('listingObj')).toBeTruthy()
     })
 
-    test ("handleCreate() should return false", () => {
+    test("handleCreate() should return false", () => {
         expect(functions.handleCreate()).toBeFalsy()
     })
 })
@@ -26,7 +21,76 @@ describe("getUserFavorites", () => {
     test("getUserFavorites should return an action with a type and payload", () => {
         expect(functions.getUserFavorites("1414")).toEqual({
             type: "GET_USER_FAVORITES",
-			payload: ["someStuff", "moreStuff", "evenMoreStuff"]
+            payload: ["someStuff", "moreStuff", "evenMoreStuff"]
         })
+    })
+})
+
+describe("handleDelete", () => {
+    test("handleDelete(listingObj) should return true", () => {
+        expect(functions.handleDelete('listingObj')).toBeTruthy()
+    })
+
+    test("handleDelete() should return false", () => {
+        expect(functions.handleDelete()).toBeFalsy()
+    })
+})
+
+describe("updateSearchTerm", () => {
+    test("updateSearchTerm should exist", () => {
+        expect(functions.updateSearchTerm).toBeTruthy();
+    })
+
+    test("updateSearchTerm with a word should return an action with a type and payload", () => {
+        expect(functions.updateSearchTerm("HELLO")).toEqual({
+            type: "UPDATE_SEARCH_TERM",
+            payload: "HELLO"
+        })
+    })
+
+    test("updateSearchTerm with an empty string should return an action with a type and payload", () => {
+        expect(functions.updateSearchTerm("")).toEqual({
+            type: "UPDATE_SEARCH_TERM",
+            payload: ""
+        })
+    })
+})
+
+describe("testSwitchCase", () => {
+    test("If backgroundColor('rgba(53, 138, 255') should return 'Electronics'", () => {
+        expect(functions.testSwitchCase('rgba(53, 138, 255')).toEqual('Electronics')
+    })
+    test("If backgroundColor('rgba(147, 74, 255') should return 'Home'", () => {
+        expect(functions.testSwitchCase('rgba(147, 74, 255')).toEqual('Home')
+    })
+    test("If backgroundColor('rgba(104, 208, 52') should return 'Sports'", () => {
+        expect(functions.testSwitchCase('rgba(104, 208, 52')).toEqual('Sports')
+    })
+    test("If backgroundColor('rgba(151, 151, 151') should return 'Parts'", () => {
+        expect(functions.testSwitchCase('rgba(151, 151, 151')).toEqual('Parts')
+    })
+    test("If backgroundColor('rgba(255, 127, 127') should return 'Free'", () => {
+        expect(functions.testSwitchCase('rgba(255, 127, 127')).toEqual('Free')
+    })
+    test("If backgroundColor('rgba(0, 255, 255, 0.68') should return 'Default'", () => {
+        expect(functions.testSwitchCase('rgba(0, 255, 255, 0.68')).toEqual('Default')
+    })
+})
+
+describe("updateFilter", () => {
+    test("If updateFilter('Electronics') should return 'Electronics'", () => {
+        expect(functions.updateFilter('Electronics')).toEqual('Electronics')
+    })
+    test("If updateFilter('Home') should return 'Home'", () => {
+        expect(functions.updateFilter('Home')).toEqual('Home')
+    })
+    test("If updateFilter('Sports') should return 'Sports'", () => {
+        expect(functions.updateFilter('Sports')).toEqual('Sports')
+    })
+    test("If updateFilter('Parts') should return 'Parts'", () => {
+        expect(functions.updateFilter('Parts')).toEqual('Parts')
+    })
+    test("If updateFilter('Free') should return 'Free'", () => {
+        expect(functions.updateFilter('Free')).toEqual('Free')
     })
 })
